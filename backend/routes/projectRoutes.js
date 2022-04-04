3555766abda22484c7276094482ac0f77167dcdd
Peter karanja <peterkaranja60@gmail.com>
2022-04-04 17:55:14 +0300

    update

---
+++ backend/routes/projectRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const {getGoals} = require('../controllers/goalControllers')
+
+// router.get('/', (req, res) => {
+//     res.status(200).json({ message: 'Get goals' });
+// })
+
+router.get('/', getGoals)
+
+router.post('/', (req, res) => {
+    res.status(200).json({ message: 'Set goals' });
+})
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({ message:  `Update goal ${req.params.id}` });
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({ message:  `Delete goal ${req.params.id}` });
+})
+
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('Hello world')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/api/goals', require('./routes/projectRoutes'))
+
+app.listen(port, () => console.log(`Server started at ${port}`));
